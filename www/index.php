8648e9c447eeaca50405d3efeb050f7e4166570b
ali <ali.issolah@eduvaud.ch>
2024-02-03 22:04:04 +0100

    index réparé

--- www/index.php
+++ www/index.php
@@ -29,7 +29,36 @@
     </header>
     <div class="content">
         <div class="content-container">
-            
+            <div class="intro">
+                <h1>SecureLinks</h1>
+                <h2 class="colortext">Want to secure your website from bots?</h2>
+                <p class="marginbot">With us, avoid getting banned. Share your links with peace of mind.</p>
+                <img src="cad.png" alt="Cadenas" class="analysis-image">
+                <p>Protect your insta</p>
+                <img src="allmylink.png" alt="allmylink" class="analysis-image">
+            </div>
+            <section class="problem-section">
+                <h2>The Problem with Instagram Bots</h2>
+                <p>Instagram bots are constantly analyzing accounts. If they find inappropriate content, they can lead to a ban. With SecureLinks, everything is protected, and nothing is detected.</p>
+                <!-- Insert image representing bots analyzing accounts here -->
+                <img src="img.png" alt="Instagram Bots Analysis" class="analysis-image">
+            </section>
+
+            <section class="solution-section">
+                <h2>How SecureLinks Protects Your Content</h2>
+                <p>We secure your links to avoid detection by bots. Create your secure website with protected links with us.</p>
+                <p>We don't include any content or keywords such as 'Private Content' or others in the raw code or on the website. Instead, we place everything on images created by our designers who write the text on the images. This prevents bots from detecting the content on the images</p>
+                <p>The links we provide do not contain any unauthorized keywords like 'Onlyfans,' 'private content,' or '+18' in the URL links. We use clean links that do not possess any unauthorized keywords.</p>
+            </section>
+
+            <section class="steps-section">
+                <h2>Easy Steps to Follow</h2>
+                <ol>
+                    <li>Create your secure website with protected links with us.</li>
+                    <li>Share the link on Instagram.</li>
+                    <li>Your content remains protected and inaccessible to bots!</li>
+                </ol>
+            </section>
         </div>
     </div>
     <footer class="footer">
