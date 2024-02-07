27d713f6680a44847ed74a1772c006af49b24935
StreallyX <77876743+StreallyX@users.noreply.github.com>
2024-02-07 08:38:29 +0100

    redirect.php v1

---
+++ www/src/anawhiterose/redirect.php
@@ -0,0 +1,12 @@
+<?php
+    if($_GET['data'] === "1"){
+        //https://mym.fans/Anawhiterose
+        echo "yes";
+        header("Location: ../create.php");
+    }
+    else if($_GET['data'] === "2"){
+        //https://onlyfans.com/ana.whiterose
+        echo "yes1";
+        header("Location: ../contact.php");
+    }
+?>
