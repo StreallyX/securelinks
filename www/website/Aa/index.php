9c4ff41800ca6de9d6dcdda71400cf2f7a750ef1
Alex <aproject37@gmail.com>
2024-02-08 21:36:57 +0100

    Changes

---
+++ www/website/Aa/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Page de Profil</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body class="background-image">
+        <div class="profile-container">
+            <img src="profilepic.png" alt="Photo de Profil" class="profile-image">
+            <h1 class="colortextw">Aa</h1>
+            <p class="colortextw">jz</p>
+        </div>
+        <div class="links-container"><a href="redirect.php?link=1">LIENhf</a><br><a href="redirect.php?link=2">LIENjfnh</a><br></div>
+        <footer>
+            <p class="footertext">SecureLinks</p>
+        </footer>
+    </body>
+</html>
